de45e72c9ae2adb3a71fa00d3c4df02d9ba0782e
François <francois.fedian.4012@free.fr>
2025-03-26 19:50:33 +0100

    Ajout d'une structure en orienté objet

--- copcoll-3.py
+++ copcoll-3.py
@@ -18,13 +18,34 @@ Description : Permet de copier rapidement des morceaux de texte prédéfinis
 """
 
 from gi.repository import Gtk
-import yaml
+import yaml  # Si tu veux intégrer YAML pour la configuration
 
-window = Gtk.Window()
-window.set_title("CopColl")
-window.set_default_size(400,300)
-label = Gtk.Label(label='Bienvenue sur CopColl')
-window.add(label)
-window.show_all()
-window.connect('delete-event', Gtk.main_quit)
+class CopColl:
+    def __init__(self):
+        self.create_window()
+
+    def create_window(self):
+        # Crée une fenêtre principale
+        window = Gtk.Window()
+        window.set_title("CopColl")
+        window.set_default_size(400, 300)
+
+        # Crée un label à afficher dans la fenêtre
+        label = Gtk.Label(label="Bienvenue sur CopColl")
+        window.add(label)
+
+        # Affiche tout
+        window.show_all()
+
+        # Gère l'événement de fermeture de la fenêtre
+        window.connect('delete-event', self.close_application)
+
+    def close_application(self, widget, event, data=None):
+        # Ferme l'application quand l'événement de fermeture est déclenché
+        Gtk.main_quit()
+
+# Crée une instance de CopColl et lance l'application
+copcoll = CopColl()
+
+# Démarre la boucle principale de GTK
 Gtk.main()
